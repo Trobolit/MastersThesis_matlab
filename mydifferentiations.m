7f47744d479e71173622a78d099e8640711ac4b7
Robert Hedman <robert.hedman@mac.com>
2019-04-23 16:44:27 +0200

    adding control law code slowly

--- mydifferentiations.m
+++ mydifferentiations.m
@@ -3,7 +3,7 @@ clear all;
 
 syms q0 q1 q2 q3 vx vy vz wx wy wz WL WR DL DR K m g rho OmegaL OmegaR ...
     deltaL deltaR udeltaR udeltaL uWR uWL Sa Sw b CL CR yac xac yt ...
-    Ix Iy Iz xf real
+    Ix Iy Iz xf v1 v2 v3 v4 real
 
 %Actuator dynamics
 WLdot = OmegaL*(-WL + uWL);
@@ -18,7 +18,7 @@ vzdot = rho*Sa*b^2*(WL^2*deltaL + WR^2*deltaR)/(2*m) ...
     - rho*b^2*(WL^2+WR^2)*Sw*(CL+CR)/(2*m) ...
     + g*(q0^2 - q1^2 -q2^2 +q3^2) ...
     - vy*wx + vx*wy;
-vdot = [vxdot, vydot, vzdot];
+vdot = [vxdot, vydot, vzdot]';
 
 wxdot = 0.5*rho*b^2*(Sa*(WR^2*deltaR-WL^2*deltaL)*yac ...
     + Sw*(WL^2 - WR^2)*yt)/Ix ...
@@ -27,26 +27,27 @@ wydot = 0.5*rho*b^2*(Sa*(WL^2*deltaL + WR^2*deltaR)*xf ...
     - Sw*(CL*WL^2+CR*WR^2)*xac)/Iy ...
     - (Ix-Iz)*wx*wz/Iy;
 wzdot = K*(WL^2 - WR^2)*yt/Iz - (Iy-Ix)*wx*wy/Iz;
-wdot = [wxdot, wydot, wzdot];
+wdot = [wxdot, wydot, wzdot]';
 
 q0dot = 0.5*(q1*wx + q2*wy + q3*wz);
 q1dot = 0.5*(q2*wz - q3*wy - q0*wx);
 q2dot = 0.5*(q3*wx - q0*wy - q1*wz);
 q3dot = 0.5*(q1*wy - q2*wx - q0*wz);
-qdot = [q0dot,q1dot,q2dot,q3dot];
+qdot = [q0dot,q1dot,q2dot,q3dot]';
 
 % States, state space equations (using above equations) and input declarations
-states = [vx vy vz wx wy wz q0 q1 q2 q3 WL WR deltaL deltaR];
-statesdot = [vdot wdot qdot WLdot WRdot deltaLdot deltaRdot];
-inputs = [uWL uWR udeltaL udeltaR];
-ydot = [vdot, wdot];
+states = [vx vy vz wx wy wz q0 q1 q2 q3 WL WR deltaL deltaR]';
+statesdot = [vdot; wdot; qdot; WLdot; WRdot; deltaLdot; deltaRdot];
+inputs = [uWL-uWR, udeltaL, udeltaR]';
+ydot = [wdot; vzdot];
+y = [wx, wy, wz, vz]';
 
 % Start differentiating, remember that y=[v,w] only.
 % ydot = [vdot, wdot] which are driectly from the state space equation, but
 % still does not contain any input, so we differentiate that one more time.
 % Lets do all states 
-ydotdot = expand(jacobian(ydot, states)*statesdot');
-J_full = jacobian(statesdot, states)*statesdot';
+ydotdot = expand(jacobian(ydot, states)*statesdot);
+%J_full = jacobian(statesdot, states)*statesdot';
 
 %collect(J,inputs(1))
 
@@ -72,26 +73,50 @@ C = {C1,C2,C3,C4};
 for j=1:numel(C)
     for i=1:numel(ydot)
         if isempty(C{j}{i})
-            Cm(j,i) = 0;
+            Cm(i,j) = 0;
         else
-            Cm(j,i) = C{j}{i};
+            Cm(i,j) = C{j}{i};
         end
     end
 end
 
-C1m = Cm(1,:);
-C2m = Cm(2,:);
-C3m = Cm(3,:);
-C4m = Cm(4,:);
+C1m = Cm(:, 1);
+C2m = Cm(:, 2);
+C3m = Cm(:, 3);
+C4m = Cm(:, 4);
+%Cm = [C1m, C2m, C3m, C4m];
 
 %% check completeness
 % idea is to remove all the input dependent terms from ydotdot and only
-% leave constant terms behind. If the Equations permit this we know what
-% was written in the pdf is true, however it does not look good.
-% Lets hope Khalid knows what to do here.
+% leave constant terms behind.
 
-noninputdependentterms = simplify(ydotdot-C1m'*uWL - C2m'*uWR - C3m'*udeltaL - C4m'*udeltaR)
+alpha = simplify(ydotdot-C1m*uWL - C2m*uWR - C3m*udeltaL - C4m*udeltaR);
+% One could find alpha by evaluating ydotdot with all inputs = 0 as well.
+beta = Cm;
+u = [uWL, uWR, udeltaL, udeltaR]';
 
-% I took the above outpuf into a text editor and searched for input terms,
-% none exist which means the equations can be written on the form
-% ydotdot = f(x) + g(x)*u, good!
+% Check sanity
+simplify(ydotdot - (alpha + beta*u)); % which is equal to 0 for all rows, good!
+
+simplify(beta*inv(beta)); % Is identity matrix?
+simplify(inv(beta)); % Check for div by 0? Only occur if an engine stop rotating.
+
+
+%% Control law
+
+% beta * inv(alpha) != 0 => good.
+
+%simplify(inv(beta*u))
+
+h = y;
+f = alpha;
+g = beta;
+
+L_gh=jacobian(h,states )*g;
+L_fh=jacobian(h,states )*f;
+L_f_gh=jacobian(L_fh, states )*g;
+L_f_fh =jacobian(L_fh, states )*f;
+
+M = L_f_gh;
+
+new_alpha = L_f_fh;
